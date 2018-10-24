007bd0993551994de88832dbec3883354d19ca97
Przemysław Bętkowski <pbetkows@grupa.lokalna>
2018-10-24 19:41:28 +0200

    Listviews, adapters, refactor

--- app/src/main/java/com/example/pbetkows/wms/MainMenuFragment.java
+++ app/src/main/java/com/example/pbetkows/wms/MainMenuFragment.java
@@ -9,11 +9,16 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
+import com.example.pbetkows.wms.goodsReceipt.AddItemsToListFragment;
 import com.example.pbetkows.wms.goodsReceipt.GoodsReceiptMainMenu;
 import com.example.pbetkows.wms.tests.SampleList;
 import com.example.pbetkows.wms.utils.DataWedgeFragment;
+import com.example.pbetkows.wms.utils.MessageBox;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,12 +26,18 @@ import butterknife.ButterKnife;
 
 public class MainMenuFragment extends Fragment {
 
-    @BindView(R.id.sample)
-    Button sampleButton;
-    @BindView(R.id.GoodsReceiptButton)
-    Button goodsReceiptButton;
-    @BindView(R.id.dataWedgeButton)
-    Button dataWedgeButton;
+//    @BindView(R.id.sample)
+//    Button sampleButton;
+//    @BindView(R.id.GoodsReceiptButton)
+//    Button goodsReceiptButton;
+//    @BindView(R.id.dataWedgeButton)
+//    Button dataWedgeButton;
+
+    @BindView(R.id.main_menu_list)
+    ListView listView;
+
+    String [] items = new String[] {"Goods Receipt", "Stock Transfer", "Data Wedge"};
+
 
 
 
@@ -35,11 +46,16 @@ public class MainMenuFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.main_menu_fragment, container, false);
         ButterKnife.bind(this, view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
 
-
-        sampleButton.setOnClickListener(v -> navigate(new SampleList()));
-        goodsReceiptButton.setOnClickListener(v -> navigate(new GoodsReceiptMainMenu()));
-        dataWedgeButton.setOnClickListener(v -> navigate(new DataWedgeFragment()));
+        listView.setOnItemClickListener((parent, view1, position, id) -> {
+            switch (items[position]) {
+                case ("Goods Receipt"): navigate(new GoodsReceiptMainMenu());break;
+                case ("Stock Transfer"): navigate(new SampleList());break;
+                case ("Data Wedge"): navigate(new DataWedgeFragment());break;
+            }
+        });
 
 
         return view;
--- app/src/main/java/com/example/pbetkows/wms/goodsReceipt/GoodsReceiptMainMenu.java
+++ app/src/main/java/com/example/pbetkows/wms/goodsReceipt/GoodsReceiptMainMenu.java
@@ -8,33 +8,52 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.pbetkows.wms.MainActivity;
 import com.example.pbetkows.wms.R;
+import com.example.pbetkows.wms.tests.SampleList;
+import com.example.pbetkows.wms.utils.DataWedgeFragment;
+import com.example.pbetkows.wms.utils.MessageBox;
 
 import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.exceptions.OnErrorNotImplementedException;
 
 public class GoodsReceiptMainMenu extends Fragment {
 
 
-    @BindView(R.id.createButton) Button createButton ;
-    @BindView(R.id.createdButton) Button createdButton;
-
-
+    @BindView(R.id.goodsReceiptMenu)
+    ListView goodsReceiptMenu;
+    String [] items = new String[] {"New Goods Receipt", "Goods Receipt Log"};
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.goods_receipt_menu, container, false);
-
         ButterKnife.bind(this, view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, items);
+        goodsReceiptMenu.setAdapter(adapter);
 
-        createButton.setOnClickListener(v -> navigate(new ChooseClientFragment()));
+        goodsReceiptMenu.setOnItemClickListener((parent, view1, position, id) -> {
+            switch (items[position]) {
+                case ("New Goods Receipt"):
+                    navigate(new ChooseClientFragment())
+                    ;break;
+                case ("Stock Transfer"):
+                    MessageBox.Show(getContext(), "Not implemented yet");
+                    break;
+            }
+        });
+
+
+
+        //createButton.setOnClickListener(v -> navigate(new ChooseClientFragment()));
 
 
         return view;
